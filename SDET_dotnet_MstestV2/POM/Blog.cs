9ef8cc9faf814393db8ccfb635d8f43b486bf56a
Sergio Guillen <sagssky@gmail.com>
2021-02-16 13:11:23 -0600

    Adding Blog and Test Case

---
+++ SDET_dotnet_MstestV2/POM/Blog.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using SDET_dotnet_MstestV2.Base;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SDET_dotnet_MstestV2.POM
+{
+    public class Blog : UnoPages
+    {
+        [FindsBy(How = How.CssSelector, Using = "li > a[href='https://blog.unosquare.com']")]
+        private IWebElement BlogLink { get; set; }
+        [FindsBy(How = How.XPath, Using = "//h1[contains(text(),'DIGITAL TRANSFORMATION BLOG')]")]
+        public IWebElement DigitalText { get; set; }
+        [FindsBy(How = How.CssSelector, Using = "form>input")]
+        public IWebElement SearchBox { get; set; }
+        [FindsBy(How = How.CssSelector, Using = "button.search-submit")]
+        public IWebElement SearchBoxClick { get; set; }
+        [FindsBy(How = How.XPath, Using = "//a[contains(text(),'Quality Assurance Questions to Ask in Agile Softwa')]")]
+        public IWebElement QABlogPost { get; set; }
+        public Blog() : base() {
+
+        }
+
+        public Blog GoToBlog() {
+            browser.Click(BlogLink);
+            return this;
+        }
+
+        public void SearchInBlog(string value) {
+            browser.WriteInElement(SearchBox, value);
+            browser.Click(SearchBoxClick);
+        }
+    }
+}
--- SDET_dotnet_MstestV2/TestCases.cs
+++ SDET_dotnet_MstestV2/TestCases.cs
@@ -7,21 +7,27 @@ namespace SDET_dotnet_MstestV2
     public class TestCases
     {
         HomePage homepage;
+        Blog blog;
         [TestInitialize]
-        public void BeforeTest()
-        {
+        public void BeforeTest() {
+            blog = new Blog();
             homepage = new HomePage();
+            //homepage.GoToServicesAndPracticeAreas();
         }
         [TestMethod]
-        public void Unosquare_BlogValidation()
-        {
-            homepage.GoToServicesAndPracticeAreas();
+        public void Unosquare_BlogValidation() {
+            blog.GoToBlog();
+            Assert.IsNotNull(blog.DigitalText);
+            blog.SearchInBlog("Quality Assurance");
+            Assert.IsNotNull(blog.QABlogPost);
+            //homepage.GoToServicesAndPracticeAreas();
             /*Add a new Page object name Blog
              *Implement all neded to use Blog in the test class (Constructor, WebElements, etc)
              *Perform the following flow, Go to www.unosquare.com
              * Go to Blog - Vlidate using Asserts, this element is present DIGITAL TRANSFORMATION BLOG
              * Search for Quality Assurance, validate with Asserts that at least one result is displayed
              */
+
         }
 
     }
